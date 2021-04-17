d2332301ad1cb1700ed8893d1f3fe9859225e7f1
kushiahu <tamayo.josue@gmail.com>
2021-04-17 13:31:51 -0500

    prepare prod

---
+++ xlsx_pdf/config/settings/prod.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = DJENV['DJ_ALLOWED_HOSTS']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': DJENV['DB_NAME'],
+		'USER': DJENV['DB_USER'],
+		'PASSWORD': DJENV['DB_PASS'],
+		'HOST': 'localhost',
+		'PORT': '5432'
+	}
+}
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+	BASE_DIR / 'static',
+]
+
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = BASE_DIR / 'static'
